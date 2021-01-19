30bbbc85aa22b5c8c966034684b368ec4b4cd21b
Hugo <becker.hugo@hotmail.com>
2021-01-19 10:02:20 +0100

    S2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import React,{useState} from 'react'
 
 function App() {
+
+const [text] = useState('Je suis un string qui utilise les fonctions hook');
   
   return (
     <div className='App'>
       <h1>Hello</h1>
+      <p>{text}</p>
     </div>
   )
 }
